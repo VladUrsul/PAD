1b4f6c896f808e98467558a970567988bac94337
VladUrsul <vladislav.ursul@isa.utm.md>
2024-10-28 21:19:27 +0200

    basic implementation

--- broker/broker.go
+++ broker/broker.go
@@ -1,1 +1,105 @@
-package broker
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"sync"
+)
+
+type Message struct {
+	Topic   string `json:"topic"`
+	Content string `json:"content"`
+	Sender  string `json:"sender"`
+}
+
+type Broker struct {
+	subscribers map[string][]net.Conn
+	mu          sync.Mutex
+}
+
+func NewBroker() *Broker {
+	return &Broker{
+		subscribers: make(map[string][]net.Conn),
+	}
+}
+
+func (b *Broker) Subscribe(topic string, conn net.Conn) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.subscribers[topic] = append(b.subscribers[topic], conn)
+}
+
+func (b *Broker) Unsubscribe(topic string, conn net.Conn) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	conns := b.subscribers[topic]
+	for i, c := range conns {
+		if c == conn {
+			b.subscribers[topic] = append(conns[:i], conns[i+1:]...)
+			break
+		}
+	}
+}
+
+func (b *Broker) Publish(msg Message) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	conns, ok := b.subscribers[msg.Topic]
+	if ok {
+		for _, conn := range conns {
+			if err := json.NewEncoder(conn).Encode(msg); err != nil {
+				fmt.Println("error sending message:", err)
+			}
+		}
+	}
+	fmt.Printf("logged message: [%s] %s: %s\n", msg.Topic, msg.Sender, msg.Content)
+}
+
+func (b *Broker) HandleConnection(conn net.Conn) {
+	defer conn.Close()
+	var action struct {
+		Type    string `json:"type"`
+		Topic   string `json:"topic,omitempty"`
+		Content string `json:"content,omitempty"`
+		Sender  string `json:"sender,omitempty"`
+	}
+
+	for {
+		if err := json.NewDecoder(conn).Decode(&action); err != nil {
+			fmt.Println("error decoding:", err)
+			return
+		}
+
+		switch action.Type {
+		case "subscribe":
+			b.Subscribe(action.Topic, conn)
+		case "unsubscribe":
+			b.Unsubscribe(action.Topic, conn)
+		case "publish":
+			msg := Message{Topic: action.Topic, Content: action.Content, Sender: action.Sender}
+			b.Publish(msg)
+		}
+	}
+}
+
+func main() {
+	broker := NewBroker()
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("error starting server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("broker listening on :8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("error accepting connection:", err)
+			continue
+		}
+		go broker.HandleConnection(conn)
+	}
+}
--- client/client.go
+++ client/client.go
@@ -1,1 +1,88 @@
-package client
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
+type Message struct {
+	Topic   string `json:"topic"`
+	Content string `json:"content"`
+	Sender  string `json:"sender"`
+}
+
+func main() {
+	var name string
+	fmt.Println("enter your name:")
+	fmt.Scanln(&name)
+
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("error connecting to broker:", err)
+		return
+	}
+	defer conn.Close()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		for {
+			var msg Message
+			if err := json.NewDecoder(conn).Decode(&msg); err == nil {
+				fmt.Printf("received message on topic '%s' from %s: %s\n", msg.Topic, msg.Sender, msg.Content)
+			}
+		}
+	}()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("enter command (subscribe, unsubscribe, publish <topic> <message>, exit):")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch {
+		case input == "subscribe":
+			fmt.Println("enter topic to subscribe:")
+			topic, _ := reader.ReadString('\n')
+			topic = strings.TrimSpace(topic)
+			action := map[string]string{"type": "subscribe", "topic": topic}
+			json.NewEncoder(conn).Encode(action)
+
+		case input == "unsubscribe":
+			fmt.Println("enter topic to unsubscribe:")
+			topic, _ := reader.ReadString('\n')
+			topic = strings.TrimSpace(topic)
+			action := map[string]string{"type": "unsubscribe", "topic": topic}
+			json.NewEncoder(conn).Encode(action)
+
+		case strings.HasPrefix(input, "publish"):
+			parts := strings.Fields(input)
+			if len(parts) < 3 {
+				fmt.Println("invalid publish command. Format: publish <topic> <message>")
+				continue
+			}
+			topic := parts[1]
+			content := strings.Join(parts[2:], " ")
+
+			action := map[string]interface{}{
+				"type":    "publish",
+				"topic":   topic,
+				"content": content,
+				"sender":  name,
+			}
+			json.NewEncoder(conn).Encode(action)
+
+		case input == "exit":
+			fmt.Println("exiting...")
+			return
+		}
+	}
+}
